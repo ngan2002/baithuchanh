131582c824062fea620805b9cdf1dc291497f308
ngan <tranthihoangan21122002@gmail.com>
2022-02-28 13:16:39 +0700

    add file java

---
+++ Baithuchanh1502/Ap51.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Ap51 {
+
+   public static void main(String[] args) throws Exception {
+    Scanner sc =new Scanner(System.in);
+    int n =0;
+    for (; n < 5;) {
+        System.out.println(n);
+        n++;
+        } 
+    }
+}
