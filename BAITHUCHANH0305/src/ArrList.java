f3d2421eb2298291c08abfba735affea2b8f4982
ngan2002 <97469000+ngan2002@users.noreply.github.com>
2022-05-03 17:40:19 +0700

    Add files via upload

---
+++ BAITHUCHANH0305/src/ArrList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+public class ArrList {
+    public static void main(String[] args){
+      ArrayList<String> arrListString  = new ArrayList<>();
+      arrListString.add("bo");
+      arrListString.add("trau");
+      arrListString.add("lon");
+      arrListString.add("ga");
+      arrListString.add("cho");
+      arrListString.add("tho");
+
+    
+     for (int i = 5; i< arrListString.size(); i++){
+        System.out.println("Cac phan tu co trong arrListstring la: ");
+     }
+     
+     
+
+    }
+
+}
